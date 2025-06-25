76ef599a80cab270fc5811c51331cfad7c94249c
Biu-Parse <b.parseghian1991@gmail.com>
2025-06-24 23:49:21 -0700

    Updating user schema to have an email and adding new before mongoose.Schema for evry existing Svhemas to modernize and avoid unexpected behavior or errors

--- models/user.js
+++ models/user.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
+    unique: true,
+    trim: true
   },
   password: {
     type: String,
     required: true,
+  },
+  email:{
+    type: String,
+    required: true,
+    unique:true,
+    lowercase: true,
+    trim: true
   },
 });
 
