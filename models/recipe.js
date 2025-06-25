35d319d14b7b788765ad70b8e487adfdac47b6a0
Biu-Parse <b.parseghian1991@gmail.com>
2025-06-24 22:40:43 -0700

    moving forward with MEN Stack Relating Data, creating the recipe schema

---
+++ models/recipe.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const recipeSchema = mongoose.Schema({
+    name: {type: string,
+         required: true
+        },
+    instructions: string,
+    owner: {type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required:true
+        },
+    ingredients:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Ingredient"
+    }
+})
+
+const Recipe = mongoose.model("Recipe", recipeSchema)
+module.exports = Recipe;
