b64dfc421e3b000c2eec3d357f8edcb72e2d08ba
Biu-Parse <b.parseghian1991@gmail.com>
2025-07-05 23:29:01 -0700

    Implementing access control check and building the index functionality

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -9,35 +9,36 @@ router.get("/", async (req, res) => {
     try {
         const userId = req.session.user?._id;
         if (!userId) {
-            return res.redirect("/auth/sign-in");
+          return res.redirect("/auth/sign-in");
         }
         const recipes = await Recipe.find({ owner: userId });
-        res.render("recipes/index.ejs", { recipes });
+        res.locals.recipes = recipes;
+        res.render("recipes/index.ejs", { recipes, currentUserId: userId });
     } catch (err) {
         console.error(err);
         res.status(500).send("Server error");
     }
 });
 
-router.get('/new', (req, res) => {
+router.get("/new", (req, res) => {
     if (!req.session.user) {
-        return res.redirect('/auth/sign-in');
+        return res.redirect("/auth/sign-in");
     }
-    res.render('recipes/new.ejs');
+    res.render("recipes/new.ejs");
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     if (!req.session.user) {
-      return res.redirect('/auth/sign-in');
+      return res.redirect("/auth/sign-in");
     }
     const newRecipe = new Recipe(req.body);
     newRecipe.owner = req.session.user._id;
     await newRecipe.save();
-    res.redirect('/recipes');
+    res.redirect("/recipes");
   } catch (error) {
-    console.error('Error creating recipe:', error);
-    res.redirect('/');
+    console.error("Error creating recipe:", error);
+    res.redirect("/");
   }
 });
 
